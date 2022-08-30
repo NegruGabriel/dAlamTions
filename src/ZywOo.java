d6e31d5b2ff4105c52655088af4f15a7109e449a
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-08-30 20:30:25 +0200

    Initial commit

---
+++ src/ZywOo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class ZywOo {
+
+    public static String howManyDalmatians(int number) {
+
+        String[] dogs = new String[] {
+                "Hardly any",
+                "More than a handful!",
+                "Woah that's a lot of dogs!",
+                "101 DALMATIANS!!!"
+        };
+
+        String respond = number <= 10
+                ? dogs[0]
+                : (number <= 50)
+                ? dogs[1]
+                : (number == 101) ? dogs[3] : dogs[2];
+
+        return respond;
+
+    }
+}
+
